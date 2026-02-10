a16dbf4104c16ff954bb23eb2f778476a1ce3e3a
Muhammad Ibtasam Sarwar <ibtasamsarwar181@gmail.com>
2026-02-10 16:01:48 +0500

    cleared

--- app/db/mongo.py
+++
@@ -1,9 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from app.core.config import MONGO_URI, DB_NAME
-
-mongo = AsyncIOMotorClient(MONGO_URI)
-db = mongo[DB_NAME]
-
-async def init_indexes():
-    await db.users.create_index("email", unique=True)
-    await db.users.create_index("tenant_id")
